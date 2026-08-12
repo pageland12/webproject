4f908bfc7fb746b16596284e75656f10bcaed9b9
pbs <barkej9@gmail.com>
2026-08-12 09:39:24 +0900

    8/12 first commit

---
+++ src/main/java/com/springboot/webproject2/Webproject2Application.java
@@ -0,0 +1,13 @@
+package com.springboot.webproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webproject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webproject2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/webproject2/Webproject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.webproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webproject2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
